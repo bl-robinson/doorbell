cc684fd743b5d299e295f95761db689f341ba39f
Ben Robinson <benr@blrobinson.uk>
2023-05-13 11:37:43 +0100

    Add outlines of what I need for a doorbell

--- ding-dong.py
+++ ding-dong.py
@@ -1,8 +1,16 @@
 import time
+from gpiozero import Button
+from signal import pause
 
-def main():
-    while True:
-        time.sleep(2)
+def button_pressed():
+    print("Ding dong ding dong")
+    print("==========================")
 
-if __name__ == '__main__':
-    main()
+def button_released():
+    print("Stop")
+    print("++++++++++++++++++++++++++")
+
+button = Button(21)
+button.when_pressed = button_pressed
+button.when_released = button_released
+pause()
