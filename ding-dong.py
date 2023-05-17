b0640bb65a19af9aab1f438396ca7c80e9f52ca3
Ben Robinson <benr@blrobinson.uk>
2023-05-17 22:20:17 +0100

    WIP doorbell api interaction

--- ding-dong.py
+++ ding-dong.py
@@ -1,6 +1,12 @@
 import time
-from gpiozero import Button
+import os
+from homeassistant_api import Client
+
 from signal import pause
+# from gpiozero import Button
+
+API_TOKEN = os.getenv('HOME_ASSISTANT_TOKEN')
+API_URL = "https://home-assistant.blrobinson.uk/api"
 
 def button_pressed():
     print("Ding dong ding dong")
@@ -10,7 +16,22 @@ def button_released():
     print("Stop")
     print("++++++++++++++++++++++++++")
 
-button = Button(21)
-button.when_pressed = button_pressed
-button.when_released = button_released
-pause()
+
+
+def main():
+    button = Button(21)
+    button.when_pressed = button_pressed
+    button.when_released = button_released
+    pause()
+
+def test_api():
+    with Client(
+        API_URL, API_TOKEN
+    ) as client:
+        bedroom_speaker = client.get_entity(slug="bedroom_speaker", group_id="media_player")
+        print(bedroom_speaker)
+        media_player = client.get_domain("media_player")
+        print(media_player.play_media(entity_id="media_player.bedroom_speaker", media_content_id="mixkit-bell-ring-buzzer-2962.wav"))
+
+if __name__ == "__main__":
+    test_api()
