912c43c5fde91a7f0f0585f0ed0ab087780dcd7b
Ben Robinson <benr@blrobinson.uk>
2023-06-07 21:21:38 +0100

    don't have a client at all times. It seems to cause the Pi1 many problems

--- ding-dong.py
+++ ding-dong.py
@@ -16,10 +16,6 @@ API_TOKEN = os.getenv('HOME_ASSISTANT_TOKEN')
 API_URL = os.getenv('HOME_ASSISTANT_API_URL')
 
 
-client = Client(
-    API_URL, API_TOKEN, cache_session=False
-)
-
 def button_pressed():
     logger.info("Ding dong ding dong")
     ring_doorbell()
@@ -36,75 +32,78 @@ def main():
     pause()
 
 def ring_doorbell():
-    media_player = client.get_domain("media_player")
+    with Client(
+        API_URL, API_TOKEN, cache_session=False
+    ) as client:
+        media_player = client.get_domain("media_player")
 
-    alexa = client.get_entity(slug="ben_s_echo_dot", group_id="media_player")
-    bedroom_speaker = client.get_entity(slug="bedroom_speaker", group_id="media_player")
-
-    logger.info("Turning Speakers On")
-    media_player.turn_on(entity_id=alexa.entity_id)
-    media_player.turn_on(entity_id=bedroom_speaker.entity_id)
-
-    alexa = client.get_entity(slug="ben_s_echo_dot", group_id="media_player")
-    bedroom_speaker = client.get_entity(slug="bedroom_speaker", group_id="media_player")
-
-    while bedroom_speaker.state.state == "off" or alexa.state.state == "off":
-        bedroom_speaker = client.get_entity(slug="bedroom_speaker", group_id="media_player")
         alexa = client.get_entity(slug="ben_s_echo_dot", group_id="media_player")
-        time.sleep(0.1)
-        logger.warn("Waiting for speakers to turn on")
+        bedroom_speaker = client.get_entity(slug="bedroom_speaker", group_id="media_player")
 
-    starting_volume_bedroom = bedroom_speaker.state.attributes['volume_level']
-    starting_volume_alexa = alexa.state.attributes['volume_level']
+        logger.info("Turning Speakers On")
+        media_player.turn_on(entity_id=alexa.entity_id)
+        media_player.turn_on(entity_id=bedroom_speaker.entity_id)
 
-    logger.info("Setting Volume to the Max")
-    media_player.volume_set(
-        entity_id=bedroom_speaker.entity_id,
-        volume_level=1
-    )
-    media_player.volume_set(
-        entity_id=alexa.entity_id,
-        volume_level=1
-    )
+        alexa = client.get_entity(slug="ben_s_echo_dot", group_id="media_player")
+        bedroom_speaker = client.get_entity(slug="bedroom_speaker", group_id="media_player")
 
-    logger.info("Playing Doorbell")
-    media_player.play_media(
-        entity_id=bedroom_speaker.entity_id,
-        media_content_id="media-source://media_source/local/doorbell-1.mp3",
-        media_content_type="music",
-    )
-    media_player.play_media(
-        entity_id=alexa.entity_id,
-        media_content_id="amzn_sfx_doorbell_chime_01",
-        media_content_type="sound"
-    )
-    time.sleep(2)
-    media_player.play_media(
-        entity_id=bedroom_speaker.entity_id,
-        media_content_id="media-source://media_source/local/doorbell-1.mp3",
-        media_content_type="music",
-    )
-    media_player.play_media(
-        entity_id=alexa.entity_id,
-        media_content_id="amzn_sfx_doorbell_chime_01",
-        media_content_type="sound"
-    )
-    time.sleep(2)
+        while bedroom_speaker.state.state == "off" or alexa.state.state == "off":
+            bedroom_speaker = client.get_entity(slug="bedroom_speaker", group_id="media_player")
+            alexa = client.get_entity(slug="ben_s_echo_dot", group_id="media_player")
+            time.sleep(0.1)
+            logger.warn("Waiting for speakers to turn on")
 
-    logger.info(f"Reset Volume")
-    media_player.volume_set(
-        entity_id=bedroom_speaker.entity_id,
-        volume_level=starting_volume_bedroom
-    )
-    media_player.volume_set(
-        entity_id=alexa.entity_id,
-        volume_level=starting_volume_alexa
-    )
+        starting_volume_bedroom = bedroom_speaker.state.attributes['volume_level']
+        starting_volume_alexa = alexa.state.attributes['volume_level']
+
+        logger.info("Setting Volume to the Max")
+        media_player.volume_set(
+            entity_id=bedroom_speaker.entity_id,
+            volume_level=1
+        )
+        media_player.volume_set(
+            entity_id=alexa.entity_id,
+            volume_level=1
+        )
+
+        logger.info("Playing Doorbell")
+        media_player.play_media(
+            entity_id=bedroom_speaker.entity_id,
+            media_content_id="media-source://media_source/local/doorbell-1.mp3",
+            media_content_type="music",
+        )
+        media_player.play_media(
+            entity_id=alexa.entity_id,
+            media_content_id="amzn_sfx_doorbell_chime_01",
+            media_content_type="sound"
+        )
+        time.sleep(2)
+        media_player.play_media(
+            entity_id=bedroom_speaker.entity_id,
+            media_content_id="media-source://media_source/local/doorbell-1.mp3",
+            media_content_type="music",
+        )
+        media_player.play_media(
+            entity_id=alexa.entity_id,
+            media_content_id="amzn_sfx_doorbell_chime_01",
+            media_content_type="sound"
+        )
+        time.sleep(2)
+
+        logger.info(f"Reset Volume")
+        media_player.volume_set(
+            entity_id=bedroom_speaker.entity_id,
+            volume_level=starting_volume_bedroom
+        )
+        media_player.volume_set(
+            entity_id=alexa.entity_id,
+            volume_level=starting_volume_alexa
+        )
 
 
-    logger.info("Turning Speakers off")
-    media_player.turn_off(entity_id=bedroom_speaker.entity_id)
-    media_player.turn_off(entity_id=alexa.entity_id)
+        logger.info("Turning Speakers off")
+        media_player.turn_off(entity_id=bedroom_speaker.entity_id)
+        media_player.turn_off(entity_id=alexa.entity_id)
 
 if __name__ == "__main__":
     main()
